555182ad8a2fdf4a3a83d2db2ad5da6eed562526
Lysko910 <44071245+Lysko910@users.noreply.github.com>
2022-10-27 14:49:30 +0200

    test comit

--- TEST.py
+++ TEST.py
@@ -1,11 +1,3 @@
-import json
 
-with open('default_parameters.json', 'r') as f:
-    data = json.load(f)
-print(data)
-parameters = {"parametr1": data['Algoritm_1'][0],
-              "parametr2": data['Algoritm_1'][1],
-              "parametr3": data['Algoritm_2'][0],
-              "parametr4": data['Algoritm_2'][1]}
 
-print(parameters)
+print("CO jest grane")
